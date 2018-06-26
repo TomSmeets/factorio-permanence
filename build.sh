d9423840c0d4d978559c1ee6e0d397d18215aa46
Tom Smeets <tom@tsmeets.nl>
2018-06-26 17:38:18 +0200

    Change directory structure

--- build.sh
+++ build.sh
@@ -1,3 +1,1 @@
-sudo rsync -avu --delete --chown=game:game ./. "/home/game/.factorio/mods/permanence_0.1.0/"
-
-zip -r 'Permanence_0.1.0.zip' ./. -x \*.sh
+zip -v -r 'Permanence_0.1.0.zip' ./Permanence_0.1.0
