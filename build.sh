cc6fe99faac6ef824791be99450688ca23e7e5dd
TomSmeets <tom@tsmeets.nl>
2019-06-27 12:31:12 +0200

    Add default.nix

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ VERSION="$(jq -r '.version' ./src/info.json)"
 mkdir build
 pushd build
 rsync -av ../src/ ./Permanence_$VERSION/
+cp ../icon.png ./Permanence_$VERSION/thumbnail.png
 zip -v -r Permanence_$VERSION.zip ./Permanence_$VERSION
 popd
