233fa18d58d99cae765de900dbe5dfb52389e810
Tom Smeets <tom@tsmeets.nl>
2024-11-04 22:21:33 +0100

    Update to factorio 2.0

--- src/control.lua
+++ src/control.lua
@@ -1,5 +1,5 @@
--- control.lua - makes objects permanent
--- Copyright 2023 Tom Smeets <tom@tsmeets.nl>
+-- control.lua - Make all placed entities permanent
+-- Copyright 2024 Tom Smeets <tom@tsmeets.nl>
 
 -- These entitles are still minable, everything else is not minable
 local ignored_types = {
@@ -9,13 +9,13 @@ local ignored_types = {
 local function handle_built_event(event)
   -- skip if the type is ignored
   for i,t in ipairs(ignored_types) do
-    if event.created_entity.type == t then
+    if event.entity.type == t then
       return
     end
   end
 
   -- tell Factorio that the tile cannot be mined
-  event.created_entity.minable = false;
+  event.entity.minable = false;
 end
 
 -- handle both player and robot built entities
