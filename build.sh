0d0e0469b0c5a157363a0db855d29d57750ba947
Tom Smeets <tom@tsmeets.nl>
2024-11-04 22:10:32 +0100

    Update build script and mod version

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
-VERSION="$(jq -r '.version' ./src/info.json)"
+set -euo pipefail
 
-mkdir build
-pushd build
-rsync -av ../src/ ./Permanence_$VERSION/
-cp ../icon.png ./Permanence_$VERSION/thumbnail.png
+# Mod version
+VERSION="0.1.7"
+echo "Mod version $VERSION"
+
+# Create a new and empty output directory
+rm -rf out
+mkdir out
+
+# Prepare files for a new zip package
+cp -rv src/     ./out/Permanence_$VERSION/
+cp     icon.png ./out/Permanence_$VERSION/thumbnail.png
+
+# Enter Build directory
+cd out
+
+# Create the zip
 zip -v -r Permanence_$VERSION.zip ./Permanence_$VERSION
-popd
+echo "Created: out/Permanence_$VERSION.zip"
